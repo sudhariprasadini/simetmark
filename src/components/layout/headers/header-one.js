37e2ec39ed6c480d660f262999a07d37b5f66cd3
sudhariprasadini <prasadinisudhari@gmail.com>
2025-09-16 00:57:08 +0530

    nav bar changes done

--- src/components/layout/headers/header-one.js
+++ src/components/layout/headers/header-one.js
@@ -1,56 +1,53 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import logo1 from "../../assets/img/logo-1.png";
-import MainMenu from './header-menu';
-import Search from './search';
-import { useState } from 'react';
-import SideBar from './offcanvas';
-import MobileMenuOne from './menu_sidebar/menu-one';
+import MainMenu from "./header-menu";
+import { useState } from "react";
+import SideBar from "./offcanvas";
+import MobileMenuOne from "./menu_sidebar/menu-one";
 
 const HeaderOne = () => {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-    const [menuSidebar, setMenuSidebar] = useState(false);
-    const [search, setSearch] = useState(false);
-    return (
-        <>
-            <div className="header__area">
-                <div className="custom_container">
-                    <div className="header__area-menubar">
-                        <div className="header__area-menubar-left one">
-                            <div className="header__area-menubar-left-logo">
-                                <Link to='/'><img src={logo1} alt='logo' /></Link>
-                            </div>
-                        </div>
-                        <div className="header__area-menubar-center">
-                            <div className="header__area-menubar-center-menu">
-                                <MainMenu />
-                            </div>
-                        </div>
-                        <div className="header__area-menubar-right">
-                            <div className="header__area-menubar-right-search">
-                                <div className="search">	
-                                    <span className="header__area-menubar-right-search-icon open" onClick={() => setSearch(true)}><i className="fal fa-search"></i></span>
-                                </div>
-                                <Search isOpen={search} setIsOpen={setSearch} />
-                            </div>
-                            <div className="header__area-menubar-right-btn one">
-                                <Link className="build_button" to="/request-quote">Get Started<i className="flaticon-right-up"></i></Link>
-                            </div>
-                            <div className="header__area-menubar-right-sidebar">
-                                <div className="header__area-menubar-right-sidebar-icon" onClick={() => setSidebarOpen(true)}>
-                                    <i className="flaticon-menu-6"></i>
-                                </div>
-                            </div>
-                            <div className="header__area-menubar-right-responsive-menu menu__bar">
-                                <i className="flaticon-menu-3" onClick={() => setMenuSidebar(true)}></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [menuSidebar, setMenuSidebar] = useState(false);
+  return (
+    <>
+      <div className="header__area">
+        <div className="custom_container">
+          <div className="header__area-menubar">
+            <div className="header__area-menubar-left one">
+              <div className="header__area-menubar-left-logo">
+                <Link to="/">
+                  <img src={logo1} alt="logo" />
+                </Link>
+              </div>
             </div>
-            <SideBar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
-            <MobileMenuOne isOpen={menuSidebar} setIsOpen={setMenuSidebar} />
-        </>
-    );
+            <div className="header__area-menubar-center">
+              <div className="header__area-menubar-center-menu">
+                <MainMenu />
+              </div>
+            </div>
+            <div className="header__area-menubar-right">
+              <div className="header__area-menubar-right-sidebar">
+                <div
+                  className="header__area-menubar-right-sidebar-icon"
+                  onClick={() => setSidebarOpen(true)}
+                >
+                  <i className="flaticon-menu-6"></i>
+                </div>
+              </div>
+              <div className="header__area-menubar-right-responsive-menu menu__bar">
+                <i
+                  className="flaticon-menu-3"
+                  onClick={() => setMenuSidebar(true)}
+                ></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <SideBar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
+      <MobileMenuOne isOpen={menuSidebar} setIsOpen={setMenuSidebar} />
+    </>
+  );
 };
 
 export default HeaderOne;
--- src/components/pages/homes/home/banner.js
+++ src/components/pages/homes/home/banner.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React from "react";
 import bannerBg from "../../../assets/img/shape/banner-shape.png";
 import user from "../../../assets/img/avatar/user.png";
 import award from "../../../assets/img/shape/award.png";
 import image from "../../../assets/img/banner/banner.jpg";
+import { Link } from "react-router-dom";
 
 const BannerOne = () => {
-    return (
-        <div className="banner__one" style={{backgroundImage: `url(${bannerBg})`}}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-12">
-                        <div className="banner__one-content">
-                            <h1 className="wow fadeInRight" data-wow-delay=".4s">Building Construction Solutions</h1>
-                            <div className="banner__one-content-user wow fadeInUp" data-wow-delay=".6s">
-                                <img src={user} alt="image" />
-                                <h5>More then 25K Clients Reviews</h5>
-                            </div>
-                            <div className="banner__one-content-award bounce_y">
-                                <img src={award} alt="image" />
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div
+      className="banner__one"
+      style={{ backgroundImage: `url(${bannerBg})` }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-12">
+            <div className="banner__one-content">
+              <h1 className="wow fadeInRight" data-wow-delay=".4s">
+                Precision Engineering for a Stronger Tomorrow
+              </h1>
+              <div
+                className="banner__one-content-user wow fadeInUp"
+                data-wow-delay=".6s"
+              >
+                <div className="wow fadeInDown" data-wow-delay="1.2s">
+                  <Link className="build_button mt-50" to="/request-quote">
+                    Start Today<i className="flaticon-right-up"></i>
+                  </Link>
                 </div>
+                <div>
+                  <div>
+                    <h5>CNC Machining</h5>
+                  </div>
+                  <div>
+                    <h5>Steel Forging</h5>
+                  </div>
+                  <div>
+                    <h5>Advanced Manufacturing</h5>
+                  </div>
+                </div>
+              </div>
+              <div className="banner__one-content-award bounce_y">
+                <img src={award} alt="image" />
+              </div>
             </div>
-            <div className="banner__one-image">
-                <img className="img_full" src={image} alt="image" />
-            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="banner__one-image">
+        <img className="img_full" src={image} alt="image" />
+      </div>
+    </div>
+  );
 };
 
 export default BannerOne;
